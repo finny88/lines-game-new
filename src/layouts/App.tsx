7ea5eed64ba5dbf33fd3e0d378ac936d4e95a1d9
e.finogeev <e.finogeev@iteco-inno.ru>
2020-05-13 16:22:30 +0300

    Come back react-hot-loader

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { hot } from 'react-hot-loader/root';
 
 const App: React.FC = () => {
   return <div className="lines-app">Lines game</div>;
 };
 
-export default App;
+export default hot(App);
