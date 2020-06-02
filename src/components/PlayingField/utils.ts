1566dea5424347873efac46dd18b9a5eb7acd988
e.finogeev <finny88@mail.ru>
2020-06-02 18:37:19 +0300

    Make squaresDecades readonly

--- src/components/PlayingField/utils.ts
+++ src/components/PlayingField/utils.ts
@@ -4,10 +4,11 @@ import { PLAYING_FIELD_ROW_SQUARES_NUMBER } from 'constants/gameCharacteristics'
 
 const decade = [...Array(PLAYING_FIELD_ROW_SQUARES_NUMBER).keys()];
 
-export const squaresDecades: IPlayingFieldSquare[][] = decade.map((i: number) =>
-  decade.map((j: number) => ({
-    row: i,
-    column: j,
-    flatIndex: i * PLAYING_FIELD_ROW_SQUARES_NUMBER + j,
-  })),
+export const squaresDecades: ReadonlyArray<readonly IPlayingFieldSquare[]> = decade.map(
+  (i: number) =>
+    decade.map((j: number) => ({
+      row: i,
+      column: j,
+      flatIndex: i * PLAYING_FIELD_ROW_SQUARES_NUMBER + j,
+    })),
 );
