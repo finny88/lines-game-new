b1953adcb74f03ba4b228c970967e3049226e52c
e.finogeev <finny88@mail.ru>
2020-11-04 09:55:49 +0300

    Кэширование непроходимых комбинаций

--- src/store/circleMoving/sagas.ts
+++ src/store/circleMoving/sagas.ts
@@ -11,29 +11,26 @@ import { batchActions } from 'redux-batched-actions';
 
 import { ICircleMotionAction } from './types';
 
-import { CircleColor } from 'constants/circleColor';
 import { CIRCLE_BLINK_ANIMATION_DELAY } from 'constants/gameCharacteristics';
-import {
-  fieldCirclesSelector,
-  NEW_FIELD_CIRCLES_GENERATED,
-  DETECT_LINES,
-} from 'store/fieldCircles';
+import { NEW_FIELD_CIRCLES_GENERATED, DETECT_LINES } from 'store/fieldCircles';
 import {
   RESET_INACCESSIBLE_DESTINATION,
   SET_INACCESSIBLE_DESTINATION,
 } from 'store/inaccessibleDestination';
 import { INIT_NEXT_POSITION } from 'store/common';
-import { getCirclesSnapshotsList } from 'utils/circleMotion';
+import { IPlayingFieldSnapshot } from 'utils/circleMotion';
 import { SET_CIRCLE_MOVING, MOVE_CIRCLE } from './actionTypes';
+import { fieldCirclesMovingSnapshotsSelector } from './selectors';
 
 type MoveCircleEffect = SelectEffect | CallEffect | PutEffect;
 
 export function* moveCircle({
   payload: circleMotion,
 }: ICircleMotionAction): Generator<MoveCircleEffect> {
-  const fieldCirclesColors = (yield select(fieldCirclesSelector)) as CircleColor[];
-
-  const snapshots = getCirclesSnapshotsList(circleMotion, fieldCirclesColors);
+  const snapshots = (yield select(
+    fieldCirclesMovingSnapshotsSelector,
+    circleMotion,
+  )) as IPlayingFieldSnapshot[];
 
   if (snapshots.length > 0) {
     for (const snapshot of snapshots) {
--- src/store/circleMoving/selectors.ts
+++ src/store/circleMoving/selectors.ts
@@ -1,3 +1,42 @@
+import { shallowEqual } from 'react-redux';
+import { createSelectorCreator, defaultMemoize } from 'reselect';
+import createCachedSelector from 're-reselect';
+
+import { CircleColor } from 'constants/circleColor';
+import { fieldCirclesSelector } from 'store/fieldCircles';
 import { IAppState } from 'store/common';
+import { getCirclesSnapshotsList, ICircleMotion } from 'utils/circleMotion';
 
 export const isCircleMovingSelector = (state: IAppState): boolean => state.isCircleMoving;
+
+const isEqual = <T>(objectA: T, objectB: T): boolean => {
+  if (Array.isArray(objectA) && Array.isArray(objectB)) {
+    return objectA === objectB;
+  }
+
+  return shallowEqual(objectA, objectB);
+};
+
+const createShallowEqualSelector = createSelectorCreator(defaultMemoize, isEqual);
+
+// Кэширование непроходимых комбинаций
+export const fieldCirclesMovingSnapshotsSelector = createCachedSelector(
+  fieldCirclesSelector,
+  (_: IAppState, circleMotion: ICircleMotion) => circleMotion,
+  // Расчет результата: набора расстановок кружков на игровом поле во время движения кружка
+  (fieldCircles: CircleColor[], circleMotion: ICircleMotion) => {
+    const snapshots = getCirclesSnapshotsList(circleMotion, fieldCircles);
+
+    // Если движение возможно, сбрасываем кэш
+    if (snapshots.length > 0) {
+      fieldCirclesMovingSnapshotsSelector.clearCache();
+    }
+
+    return snapshots;
+  },
+)({
+  keySelector: (_: IAppState, circleMotion: ICircleMotion) =>
+    /** Расчет ключа для кеширования */
+    JSON.stringify(circleMotion),
+  selectorCreator: createShallowEqualSelector,
+});
